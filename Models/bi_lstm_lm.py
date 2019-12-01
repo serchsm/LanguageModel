03f5a84c18e6f07e1aa3b60497a47024137039b9
Sergio Murillo <>
2019-12-01 12:48:38 -0500

    Bidirectional LSTM implementation

--- Models/bi_lstm_lm.py
+++ Models/bi_lstm_lm.py
@@ -1,9 +1,19 @@
 import tensorflow as tf
 
+
 class BiLSTMLanguageModel(tf.keras.Model):
 
     def __init__(self, vocabulary_size, args):
         super(BiLSTMLanguageModel, self).__init__()
-        self.embbeding_size = args.embedding_size
-        
+        self.embedding = tf.keras.layers.Embedding(
+            vocabulary_size, args.embedding_size)
+        self.biLSTM = tf.keras.layers.Bidirectional(
+            tf.keras.layers.LSTM(args.number_hidden))
+        self.w = tf.keras.layers.Dense(vocabulary_size)
+
+    def call(x_in):
+        # Word index to embeddings
+        x = self.embedding(x)
+        output, state = self.biLSTM(x)
+        return self.w(output)
 
--- lm.py
+++ lm.py
@@ -1,13 +1,11 @@
 import argparse
 
-from Models.bi_lstim_lm.BiLSTMLanguageModel import biLSTM_LM
+from Models.bi_lstm_lm import BiLSTMLanguageModel as biLSTM_LM
 
 def main(args):
-    
     vocabulary_size = 1024
     print(f"model: {args.model}, embedding_size: {args.embedding_size}")
     biLSTM_lm = biLSTM_LM(vocabulary_size, args)
-    print(f"embedding_size: {biLSTM_lm.embedding_size}")
 
 
 if __name__ == "__main__":
@@ -15,7 +13,7 @@ if __name__ == "__main__":
     parser.add_argument('--model', type=str, default='bi_lstm', help = ' bi_lstm | lstm')
     parser.add_argument('--embedding_size', type=int, default = 256, help = 'embedding size')
     parser.add_argument('--number_layers', type=int, default=2, help='number layers')
-    parser.add_argument('--number_hidden, type=int', default=2, help='number hidden units')
+    parser.add_argument('--number_hidden', type=int, default=512, help='number hidden units')
     args = parser.parse_args()
 
     main(args)
