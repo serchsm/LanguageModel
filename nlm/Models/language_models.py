5ea2acdafd56c416ab413fa6c1bac1c72ed89be4
Sergio Murillo <serchsm@users.noreply.github.com>
2021-07-08 17:26:59 -0400

    Simple lstm lm and edit command line arguments

--- nlm/Models/language_models.py
+++ nlm/Models/language_models.py
@@ -1,16 +1,23 @@
 import tensorflow as tf
 
 
-class GRULM(tf.keras.Model):
+def ngram_lstm(vocabulary_size, embedding_size=32, lstm_units=128):
+    return tf.keras.models.Sequential([
+        tf.keras.layers.Embedding(vocabulary_size, embedding_size),
+        tf.keras.layers.LSTM(lstm_units),
+        tf.keras.layers.Dense(vocabulary_size, activation='softmax'),
+    ])
+
+class LSTMLM(tf.keras.Model):
     def __init__(self, vocabulary_size, embedding_size=128, number_hidden=256):
-        super(GRULM, self).__init__()
+        super(LSTMLM, self).__init__()
         self.embedding = tf.keras.layers.Embedding(vocabulary_size, embedding_size)
-        self.gru = tf.keras.layers.GRU(number_hidden)
+        self.lstm= tf.keras.layers.LSTM(number_hidden)
         self.fc = tf.keras.layers.Dense(vocabulary_size)
     
     def call(self, x_in):
        # Word index to embedding
        x = self.embedding(x_in)
        print(f"emb shape: {x.shape}")
-       output = self.gru(x)
+       output = self.lstm(x)
        return self.fc(output)
--- nlm/lm.py
+++ nlm/lm.py
@@ -2,11 +2,11 @@ import argparse
 import numpy as np
 import tensorflow as tf
 
-# import utilities as utils
+from sklearn.model_selection import train_test_split
+
 from helpers import utilities
 from Tokenizer.tokenizer import TextTokenizer
-# from Models.bi_lstm_lm import GRULM
-# from Processor.data_processor import get_data
+from Models.language_models import ngram_lstm
 
 def main(args):
     text = utilities.get_text_corpus()
@@ -19,6 +19,10 @@ def main(args):
     batched_dataset = utilities.create_batched_inputs_targets(args.batch_size, windows)
     for x, y in batched_dataset.take(1):
         print(f"x: {x}, y: {y}")
+    vocabulary_size = len(tokenizer.tokenizer.word_index)
+    ngram_lm = ngram_lstm(vocabulary_size, args.embedding_size, args.lstm_units) 
+    ngram_lm.summary()
+
 
     # vocabulary_size = 1024
     # gru_lm = GRULM(vocabulary_size)
@@ -34,10 +38,10 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--ngram', type=int, default=4, help='Ngram order')
     parser.add_argument('--batch_size', type=int, default=4, help='Batch size to train model')
-    parser.add_argument('--model', type=str, default='bi_lstm', help = ' bi_lstm | lstm')
-    parser.add_argument('--embedding_size', type=int, default = 256, help = 'embedding size')
-    parser.add_argument('--number_layers', type=int, default=2, help='number layers')
-    parser.add_argument('--number_hidden', type=int, default=512, help='number hidden units')
+    # parser.add_argument('--model', type=str, default='bi_lstm', help = ' bi_lstm | lstm')
+    parser.add_argument('--embedding_size', type=int, default = 32, help = 'embedding size')
+    # parser.add_argument('--number_layers', type=int, default=2, help='number layers')
+    parser.add_argument('--lstm_units', type=int, default=256, help='number lstm units')
     args = parser.parse_args()
 
     main(args)
