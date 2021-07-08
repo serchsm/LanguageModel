ba77640636237842a8ac7f227bfdae41f1fc87b8
Sergio Murillo <serchsm@users.noreply.github.com>
2021-07-08 18:00:28 -0400

    Trains toy example

--- nlm/lm.py
+++ nlm/lm.py
@@ -15,14 +15,17 @@ def main(args):
     encoded_text = tokenizer.encode_text(text)[0]
     
     windows = utilities.generate_windows(args.ngram, encoded_text)
+    train_windows, validation_windows = train_test_split(windows, test_size=0.1, random_state=42)
     print(f"w_length: {args.ngram}, windows: {windows[:10]}")
-    batched_dataset = utilities.create_batched_inputs_targets(args.batch_size, windows)
-    for x, y in batched_dataset.take(1):
-        print(f"x: {x}, y: {y}")
+    
+    training_dataset = utilities.create_batched_inputs_targets(args.batch_size, train_windows[:100])
+    validation_dataset = utilities.create_batched_inputs_targets(args.batch_size, validation_windows[:10])
+    
     vocabulary_size = len(tokenizer.tokenizer.word_index)
     ngram_lm = ngram_lstm(vocabulary_size, args.embedding_size, args.lstm_units) 
+    ngram_lm.compile(loss="sparse_categorical_crossentropy", optimizer="adam", metrics=['acc'])
     ngram_lm.summary()
-
+    ngram_lm.fit(training_dataset, epochs=args.epochs, validation_data=validation_dataset)
 
     # vocabulary_size = 1024
     # gru_lm = GRULM(vocabulary_size)
@@ -37,6 +40,7 @@ def main(args):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--ngram', type=int, default=4, help='Ngram order')
+    parser.add_argument('--epochs', type=int, default=20, help='Number epochs')
     parser.add_argument('--batch_size', type=int, default=4, help='Batch size to train model')
     # parser.add_argument('--model', type=str, default='bi_lstm', help = ' bi_lstm | lstm')
     parser.add_argument('--embedding_size', type=int, default = 32, help = 'embedding size')
