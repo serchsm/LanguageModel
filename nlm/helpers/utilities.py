45a3b7ddfa431f7d7c795eab8f48af36c6368218
Sergio Murillo <serchsm@users.noreply.github.com>
2021-07-12 14:25:13 -0400

    [bug-fix] Compatiblily issue with TF en ece2 instance. Needs np array for features

--- nlm/helpers/utilities.py
+++ nlm/helpers/utilities.py
@@ -11,7 +11,7 @@ def get_text_corpus():
     return text
 
 def generate_windows(window_length, full_sequence):
-    return [full_sequence[i:i+window_length] for i in range(0, len(full_sequence)-window_length+1)] 
+    return np.asarray([full_sequence[i:i+window_length] for i in range(0, len(full_sequence)-window_length+1)])
 
 def split_input_target(window):
     return window[:-1], window[-1:]
