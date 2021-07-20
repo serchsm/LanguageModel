30dfb04111f0599e6aac59c4cf263c5a92e49713
Sergio Murillo <serchsm@users.noreply.github.com>
2021-07-20 17:13:34 -0400

    [refactor] Fix style

--- nlm/helpers/utilities.py
+++ nlm/helpers/utilities.py
@@ -3,6 +3,7 @@ import tensorflow as tf
 
 from pathlib import Path
 
+
 def get_text_corpus():
     path_to_file = tf.keras.utils.get_file('shakespeare.txt',
                                            'https://storage.googleapis.com/download.tensorflow.org/data/shakespeare.txt')
@@ -10,15 +11,19 @@ def get_text_corpus():
         text = fid.read()
     return text
 
+
 def generate_windows(window_length, full_sequence):
-    return np.asarray([full_sequence[i:i+window_length] for i in range(0, len(full_sequence)-window_length+1)])
+    return np.asarray([full_sequence[i:i + window_length] for i in range(0, len(full_sequence) - window_length + 1)])
+
 
 def split_input_target(window):
     return window[:-1], window[-1:]
 
+
 def create_batched_inputs_targets(batch_size, windows):
     ds = tf.data.Dataset.from_tensor_slices(windows).shuffle(len(windows))
     return ds.map(split_input_target).batch(batch_size, drop_remainder=True).prefetch(4)
+
 
 class TextGenerator():
     def __init__(self, lm, number_words, tokenizer, ngram, temperature=1.0):
@@ -27,12 +32,12 @@ class TextGenerator():
         self.tokenizer = tokenizer
         self.temperature = temperature
         self.ngram = ngram
-    
+
     def next_input(self, in_sequence):
-        return in_sequence[-self.ngram+1:]
+        return in_sequence[-self.ngram + 1:]
 
     def sample_next_word(self, predictions):
-        scaled_logits = np.log(predictions)/self.temperature
+        scaled_logits = np.log(predictions) / self.temperature
         return tf.random.categorical(np.array(scaled_logits), num_samples=1).numpy()[0]
 
     def generate_text(self, next_sequence):
