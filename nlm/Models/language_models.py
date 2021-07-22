5186dbdad289f0249670fb7a849dbd8153406c33
Sergio Murillo <serchsm@users.noreply.github.com>
2021-07-22 16:29:26 -0400

    [feature] No need to evaluate LM on evaluation set. Just learning ngram probabilities for generative model

--- nlm/Models/language_models.py
+++ nlm/Models/language_models.py
@@ -8,17 +8,19 @@ def ngram_lstm(vocabulary_size, embedding_size=32, lstm_units=128):
         tf.keras.layers.Dense(vocabulary_size, activation='softmax'),
     ])
 
-
-class LSTMLM(tf.keras.Model):
-    def __init__(self, vocabulary_size, embedding_size=128, number_hidden=256):
-        super(LSTMLM, self).__init__()
-        self.embedding = tf.keras.layers.Embedding(vocabulary_size, embedding_size)
-        self.lstm = tf.keras.layers.LSTM(number_hidden)
-        self.fc = tf.keras.layers.Dense(vocabulary_size)
-
-    def call(self, x_in):
-        # Word index to embedding
-        x = self.embedding(x_in)
-        print(f"emb shape: {x.shape}")
-        output = self.lstm(x)
-        return self.fc(output)
+def ngram_lstm_with_pretrained_embeddings(vocabulary_size, embedding_matrix, lstm_units=128):
+    embedding_values = tf.constant_initializer(embedding_matrix)
+    return tf.keras.models.Sequential([
+        tf.keras.layers.Embedding(vocabulary_size,
+                                  embedding_matrix.shape[1],
+                                  embeddings_initializer=embedding_values,
+                                  trainable=False),
+        tf.keras.layers.LSTM(lstm_units,
+                             dropout=0.2,
+                             recurrent_dropout=0.2,
+                             return_sequences=True),
+        tf.keras.layers.LSTM(lstm_units,
+                             dropout=0.2,
+                             recurrent_dropout=0.2),
+        tf.keras.layers.Dense(vocabulary_size, activation='softmax'),
+    ])
